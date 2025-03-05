7167a01a96688368898b78083e4c426ad8f689ac
Ravish Jain <168062806+ravishjain06@users.noreply.github.com>
2025-03-05 08:41:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-    origin: "http://localhost:5173",
+    origin: "https://lucky-job-frontend.vercel.app/",
     // methods:"GET,POST,PUT,DELETE",
     credentials: true,
 }
